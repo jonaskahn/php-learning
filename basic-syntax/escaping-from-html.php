be3dbcd617a88de38fccdb135d16b92c01c33b7e
Tea <tuyendev@gmail.com>
2021-04-20 11:19:39 +0700

    Update escaping-from-html.php

--- basic-syntax/escaping-from-html.php
+++ basic-syntax/escaping-from-html.php
@@ -1,4 +1,15 @@
-// Example 0
+//Example
 <p>This is going to be ignored by PHP and displayed by the browser.</p>
 <?php echo 'While this is going to be parsed.'; ?>
 <p>This will also be ignored by PHP and displayed by the browser.</p>
+
+//Examples of entering and exiting the PHP parser
+<?php
+    echo 'This is a test';
+?>
+
+<?php echo 'This is a test' ?>
+
+<?php echo 'We omitted the last closing tag';
+
+//Advanced escaping using conditions

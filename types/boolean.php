52baa8f7c82896836927eb5347795768c54322c2
Tea <tuyendev@gmail.com>
2021-04-20 14:21:06 +0700

    Update boolean.php

--- types/boolean.php
+++ types/boolean.php
@@ -15,3 +15,16 @@ if ($show_separators) {
     echo "<hr>\n";
 }
 ?>
+
+<!-- EXAMPLE DEFAULT BOOLEAN -->
+
+<?php
+var_dump((bool) "");        // bool(false)
+var_dump((bool) 1);         // bool(true)
+var_dump((bool) -2);        // bool(true)
+var_dump((bool) "foo");     // bool(true)
+var_dump((bool) 2.3e5);     // bool(true)
+var_dump((bool) array(12)); // bool(true)
+var_dump((bool) array());   // bool(false)
+var_dump((bool) "false");   // bool(true)
+?>

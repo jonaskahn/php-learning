ea90a5750dbdd020248c71f3a8855f62ec7e5891
Tea <tuyendev@gmail.com>
2021-04-20 11:20:56 +0700

    Update escaping-from-html.php

--- basic-syntax/escaping-from-html.php
+++ basic-syntax/escaping-from-html.php
@@ -3,6 +3,8 @@
 <?php echo 'While this is going to be parsed.'; ?>
 <p>This will also be ignored by PHP and displayed by the browser.</p>
 
+/*-------------------------------------------------------------------*/
+
 //Examples of entering and exiting the PHP parser
 <?php
     echo 'This is a test';
@@ -12,4 +14,12 @@
 
 <?php echo 'We omitted the last closing tag';
 
+
+?>
+/*-------------------------------------------------------------------*/
 //Advanced escaping using conditions
+<?php if ($expression == true): ?>
+  This will show if the expression is true.
+<?php else: ?>
+  Otherwise this will show.
+<?php endif; ?>

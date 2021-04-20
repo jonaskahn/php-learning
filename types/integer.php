2b07fbb6ac517df5f4f18d82d90818c6efb632ea
Tea <tuyendev@gmail.com>
2021-04-20 14:35:13 +0700

    Update integer.php

--- types/integer.php
+++ types/integer.php
@@ -1,3 +1,4 @@
+<!-- Example 1 -->
 <?php
 $a = 1234; // decimal number
 $a = 0123; // octal number (equivalent to 83 decimal)
@@ -5,3 +6,30 @@ $a = 0x1A; // hexadecimal number (equivalent to 26 decimal)
 $a = 0b11111111; // binary number (equivalent to 255 decimal)
 $a = 1_234_567; // decimal number (as of PHP 7.4.0)
 ?>
+
+<!-- Example 2. Integer overflow--> 
+<?php
+$large_number = 2147483647;
+var_dump($large_number);                     // int(2147483647)
+
+$large_number = 2147483648;
+var_dump($large_number);                     // float(2147483648)
+
+$million = 1000000;
+$large_number =  50000 * $million;
+var_dump($large_number);                     // float(50000000000)
+?>
+
+<!--Example #3 Integer overflow on a 64-bit system -->
+<?php
+$large_number = 9223372036854775807;
+var_dump($large_number);                     // int(9223372036854775807)
+
+$large_number = 9223372036854775808;
+var_dump($large_number);                     // float(9.2233720368548E+18)
+
+$million = 1000000;
+$large_number =  50000000000000 * $million;
+var_dump($large_number);                     // float(5.0E+19)
+?>
+

4f7cab0641cdc235145ed6dc1cb3c997247976a3
Tea <tuyendev@gmail.com>
2021-04-20 11:32:54 +0700

    Update escaping-from-html.php

--- basic-syntax/escaping-from-html.php
+++ basic-syntax/escaping-from-html.php
@@ -1,13 +1,11 @@
-<h1>Example 1</h1>
+<!-- Example 1 -->
 
 <p>This is going to be ignored by PHP and displayed by the browser.</p>
 <?php echo 'While this is going to be parsed.'; ?>
 <p>This will also be ignored by PHP and displayed by the browser.</p>
 
 /*-------------------------------------------------------------------*/
-
-<h1>Examples of entering and exiting the PHP parser </h1>
-
+<!-- Examples of entering and exiting the PHP parser -->
 <?php
     echo 'This is a test';
 ?>
@@ -20,7 +18,7 @@
 ?>
 /*-------------------------------------------------------------------*/
 
-<h1>3. Advanced escaping using conditions</h1>
+<!-- 3. Advanced escaping using conditions -->
 
 <?php if ($expression == true): ?>
   This will show if the expression is true.

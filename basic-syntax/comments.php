41890bec93b5c7bdbf6ca6c26c51f6dc8067933f
Tea <tuyendev@gmail.com>
2021-04-20 11:30:40 +0700

    Create comments.php

---
+++ basic-syntax/comments.php
@@ -0,0 +1,36 @@
+<?php
+    echo 'This is a test'; // This is a one-line c++ style comment
+
+    /* This is a multi line comment
+       yet another line of comment */
+    echo 'This is yet another test';
+
+    echo 'One Final Test'; # This is a one-line shell-style comment
+?>
+
+<h1>Another style</h1>
+
+<?php
+
+//======================================================================
+// CATEGORY LARGE FONT
+//======================================================================
+
+//-----------------------------------------------------
+// Sub-Category Smaller Font
+//-----------------------------------------------------
+
+/* Title Here Notice the First Letters are Capitalized */
+
+# Option 1
+# Option 2
+# Option 3
+
+/*
+* This is a detailed explanation
+* of something that should require
+* several paragraphs of information.
+*/
+
+// This is a single line quote.
+?>

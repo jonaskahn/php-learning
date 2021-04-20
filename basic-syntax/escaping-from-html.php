f0235e077410199f9ac195d9c5cd416dcd3659dc
Tea <tuyendev@gmail.com>
2021-04-20 11:20:15 +0700

    Update escaping-from-html.php

--- basic-syntax/escaping-from-html.php
+++ basic-syntax/escaping-from-html.php
@@ -1,4 +1,4 @@
-//Example
+//Example  
 <p>This is going to be ignored by PHP and displayed by the browser.</p>
 <?php echo 'While this is going to be parsed.'; ?>
 <p>This will also be ignored by PHP and displayed by the browser.</p>

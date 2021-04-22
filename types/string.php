a866adfb6fd3516213b1d2ae23e9cd9e78fa09c0
Tea <tuyendev@gmail.com>
2021-04-22 14:58:40 +0700

    Update string.php

--- types/string.php
+++ types/string.php
@@ -1,3 +1,5 @@
+<!-- Single quoted -->
+
 <?php
 echo 'this is a simple string';
 
@@ -25,5 +27,22 @@ echo 'Variables do not $expand $either';
 $num = 12345;
 echo 'number: $num';
 //Output 'number: $num' not 'number: 12345'
+?>
 
+<!-- Double quoted -->
+
+<!--
+\n	linefeed (LF or 0x0A (10) in ASCII)
+\r	carriage return (CR or 0x0D (13) in ASCII)
+\t	horizontal tab (HT or 0x09 (9) in ASCII)
+\v	vertical tab (VT or 0x0B (11) in ASCII)
+\e	escape (ESC or 0x1B (27) in ASCII)
+\f	form feed (FF or 0x0C (12) in ASCII)
+\\	backslash
+\$	dollar sign
+\"	double-quote
+\[0-7]{1,3}	the sequence of characters matching the regular expression is a character in octal notation, which silently overflows to fit in a byte (e.g. "\400" === "\000")
+\x[0-9A-Fa-f]{1,2}	the sequence of characters matching the regular expression is a character in hexadecimal notation
+\u{[0-9A-Fa-f]+}	the sequence of characters matching the regular expression is a Unicode codepoint, which will be output to the string as that codepoint's UTF-8 representation
+-->
 

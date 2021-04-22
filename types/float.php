9994b0ac3fb7a1f791e57557060766913660a3c5
Tea <tuyendev@gmail.com>
2021-04-22 14:33:27 +0700

    Update float.php

--- types/float.php
+++ types/float.php
@@ -7,3 +7,23 @@ $c = 7E-10;
 $d = 1_234.567; // as of PHP 7.4.0
 ?>
 
+<!-- Example Numberic String float -->
+<!-- 
+WHITESPACES      \s*
+LNUM             [0-9]+
+DNUM             ([0-9]*)[\.]{LNUM}) | ({LNUM}[\.][0-9]*)
+EXPONENT_DNUM    (({LNUM} | {DNUM}) [eE][+-]? {LNUM})
+INT_NUM_STRING   {WHITESPACES} [+-]? {LNUM} {WHITESPACES}
+FLOAT_NUM_STRING {WHITESPACES} [+-]? ({DNUM} | {EXPONENT_DNUM}) {WHITESPACES}
+NUM_STRING       ({INT_NUM_STRING} | {FLOAT_NUM_STRING})
+-->
+<?php
+$foo = 1 + "10.5";                // $foo is float (11.5)
+$foo = 1 + "-1.3e3";              // $foo is float (-1299)
+$foo = 1 + "bob-1.3e3";           // TypeError as of PHP 8.0.0, $foo is integer (1) previously
+$foo = 1 + "bob3";                // TypeError as of PHP 8.0.0, $foo is integer (1) previously
+$foo = 1 + "10 Small Pigs";       // $foo is integer (11) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+$foo = 4 + "10.2 Little Piggies"; // $foo is float (14.2) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+$foo = "10.0 pigs " + 1;          // $foo is float (11) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+$foo = "10.0 pigs " + 1.0;        // $foo is float (11) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+?>

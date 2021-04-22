f9cd68fbd9a495e394ab0329faad03abfc0870f2
Tea <tuyendev@gmail.com>
2021-04-22 16:00:01 +0700

    Update array.php

--- types/array.php
+++ types/array.php
@@ -72,3 +72,69 @@ array(4) {
   string(5) "world"
 }
 -->
+
+
+<!-- Keys not on all elements -->
+<?php
+$array = array(
+         "a",
+         "b",
+    6 => "c",
+         "d",
+);
+var_dump($array);
+?>
+
+<!-- RESULT 
+array(4) {
+  [0]=>
+  string(1) "a"
+  [1]=>
+  string(1) "b"
+  [6]=>
+  string(1) "c"
+  [7]=>
+  string(1) "d"
+}
+-->
+
+
+<!--  Complex Type Casting and Overwriting example -->
+
+<?php
+$array = array(
+    1    => 'a',
+    '1'  => 'b', // the value "a" will be overwritten by "b"
+    1.5  => 'c', // the value "b" will be overwritten by "c"
+    -1 => 'd',
+    '01'  => 'e', // as this is not an integer string it will NOT override the key for 1
+    '1.5' => 'f', // as this is not an integer string it will NOT override the key for 1
+    true => 'g', // the value "c" will be overwritten by "g"
+    false => 'h',
+    '' => 'i',
+    null => 'j', // the value "i" will be overwritten by "j"
+    'k', // value "k" is assigned the key 2. This is because the largest integer key before that was 1
+    2 => 'l', // the value "k" will be overwritten by "l"
+);
+
+var_dump($array);
+?>
+
+<!-- RESULT 
+array(7) {
+  [1]=>
+  string(1) "g"
+  [-1]=>
+  string(1) "d"
+  ["01"]=>
+  string(1) "e"
+  ["1.5"]=>
+  string(1) "f"
+  [0]=>
+  string(1) "h"
+  [""]=>
+  string(1) "j"
+  [2]=>
+  string(1) "l"
+}
+-->

2b70244569d2f63660f533a1f890ec5c4eeddcbe
Tea <tuyendev@gmail.com>
2021-04-22 15:57:15 +0700

    Update array.php

--- types/array.php
+++ types/array.php
@@ -11,3 +11,64 @@ $array = [
     "bar" => "foo",
 ];
 ?>
+
+<!-- Type Casting and Overwriting example -->
+<?php
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($array);
+?>
+
+<!-- RESULT 
+array(1) {
+  [1]=>
+  string(1) "d"
+}
+-->
+
+<!--  Mixed int and string keys -->
+<?php
+$array = array(
+    "foo" => "bar",
+    "bar" => "foo",
+    100   => -100,
+    -100  => 100,
+);
+var_dump($array);
+?>
+
+<!-- RESULT 
+array(4) {
+  ["foo"]=>
+  string(3) "bar"
+  ["bar"]=>
+  string(3) "foo"
+  [100]=>
+  int(-100)
+  [-100]=>
+  int(100)
+}
+-->
+
+<!--  Indexed arrays without key -->
+<?php
+$array = array("foo", "bar", "hello", "world");
+var_dump($array);
+?>
+
+<!-- RESULT 
+array(4) {
+  [0]=>
+  string(3) "foo"
+  [1]=>
+  string(3) "bar"
+  [2]=>
+  string(5) "hello"
+  [3]=>
+  string(5) "world"
+}
+-->

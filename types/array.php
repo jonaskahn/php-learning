b3bc6ec9c5b0bda29b4d9474c7457712d4d11924
Tea <tuyendev@gmail.com>
2021-04-22 15:40:45 +0700

    Create array.php

---
+++ types/array.php
@@ -0,0 +1,13 @@
+<!-- A simple array -->
+<?php
+$array = array(
+    "foo" => "bar",
+    "bar" => "foo",
+);
+
+// Using the short array syntax
+$array = [
+    "foo" => "bar",
+    "bar" => "foo",
+];
+?>
